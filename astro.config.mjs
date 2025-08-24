e0cd092f2cecc6b2a480de6672c90034122da5e3
Kathr1s <18.ismael.alvarez@gmail.com>
2025-08-23 22:36:07 -0500

    firebase and vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'server', 
-  adapter: vercel(),
+  output: 'static', 
 });
+
